f7d9e26b0cb08f4f47cfb52876b92876bdec7562
moratomicro <moratomicro@yahoo.com.br>
2023-06-06 09:34:09 -0300

    Commit do dia 06 de Junho de 2023

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -53,10 +53,37 @@ class UserController {
       const novosDados = await user.update(req.body);
       return res.json(novosDados);
     } catch (e) {
-      return res.json(null);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
     }
   }
+
   // Controller de Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
