3c75bf62e581ab63f28fd45b32cd4624426b0fa7
moratomicro <moratomicro@yahoo.com.br>
2023-06-07 16:03:51 -0300

    Commit do dia 07 de Julho de 2023

---
+++ src/controllers/TokenController.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+
+class TokenController {
+  async store(req, res) {
+    const { email = '', password = '' } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({
+        errors: ['Credenciais inválidas'],
+      });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuário não existe.'],
+      });
+    }
+
+    return res.json(user);
+  }
+}
+
+export default new TokenController();
