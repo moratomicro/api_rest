86b67322f51261a9deab426d0d72e98d6ebac67c
moratomicro <moratomicro@yahoo.com.br>
2023-06-20 14:47:37 -0300

    Commit do dia 20 de Junho de 2023

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Luiz Alexandre',
-      sobrenome: 'Morato',
-      email: 'luizbe@gmail.com',
-      idade: 9,
-      peso: 22,
-      altura: 1.02,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,7 +6,7 @@ import loginRequired from '../middlewares/loginRequired';
 const router = new Router();
 
 // Não deveria existir:
-router.get('/', userController.index); // Lista de usuários
+// router.get('/', userController.index); // Lista de usuários
 // router.get('/:id', userController.show); // Exibição de usuário especificado pelo id
 
 // Esse deve existir ou não, depende do cenário
